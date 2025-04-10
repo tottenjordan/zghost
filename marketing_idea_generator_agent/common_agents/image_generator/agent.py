66ce83ba098bbdb12b84b5b70bde5f4eed209a4c
Jeremy Wortz <jwortz@google.com>
2025-04-10 17:43:46 -0500

    veo-kinda-working

--- marketing_idea_generator_agent/common_agents/image_generator/agent.py
+++ marketing_idea_generator_agent/common_agents/image_generator/agent.py
@@ -1,7 +1,7 @@
-from .tools import generate_image
+from .tools import generate_image, generate_video
 from google.adk.agents import Agent
 from google.adk.tools import load_artifacts
-
+from google.adk.tools import ToolContext, LongRunningFunctionTool
 from .prompts import image_generation_instructions
 from google.genai import types
 
@@ -10,7 +10,11 @@ image_generation_agent = Agent(
     model="gemini-2.0-flash-001",
     name="image_generation_agent",
     instruction=image_generation_instructions,
-    tools=[generate_image, load_artifacts],
+    tools=[
+        generate_image,
+        load_artifacts,
+        LongRunningFunctionTool(func=generate_video),
+    ],
     generate_content_config=types.GenerateContentConfig(
         temperature=1.0,
         # response_modalities=["TEXT", "AUDIO"],
--- marketing_idea_generator_agent/common_agents/image_generator/tools.py
+++ marketing_idea_generator_agent/common_agents/image_generator/tools.py
@@ -3,25 +3,51 @@ from google.adk.tools import load_artifacts
 from google.adk.tools import ToolContext
 from google.genai import Client
 from google.genai import types
+from google.genai.types import GenerateVideosConfig, Image
+import time
+import os
 
 # Only Vertex AI supports image generation for now.
 client = Client()
 
 
-def generate_image(prompt: str, tool_context: 'ToolContext'):
-  """Generates an image based on the prompt."""
-  response = client.models.generate_images(
-      model='imagen-3.0-generate-002',
-      prompt=prompt,
-      config={'number_of_images': 1},
-  )
-  if not response.generated_images:
-    return {'status': 'failed'}
-  image_bytes = response.generated_images[0].image.image_bytes
-  tool_context.save_artifact(
-      'image.png',
-      types.Part.from_bytes(data=image_bytes, mime_type='image/png'),
-  )
-  return {'status': 'ok', 'filename': 'image.png'}
+def generate_image(prompt: str, tool_context: "ToolContext"):
+    """Generates an image based on the prompt."""
+    response = client.models.generate_images(
+        model="imagen-3.0-generate-002",
+        prompt=prompt,
+        config={"number_of_images": 1},
+    )
+    if not response.generated_images:
+        return {"status": "failed"}
+    image_bytes = response.generated_images[0].image.image_bytes
+    tool_context.save_artifact(
+        "image.png",
+        types.Part.from_bytes(data=image_bytes, mime_type="image/png"),
+    )
+    return {"status": "ok", "filename": "image.png"}
 
 
+def generate_video(prompt: str, tool_context: "ToolContext"):
+    """Generates an image based on the prompt."""
+
+    operation = client.models.generate_videos(
+        model="veo-2.0-generate-001",
+        prompt=prompt,
+        config=GenerateVideosConfig(
+            aspect_ratio="16:9",
+            output_gcs_uri=os.environ.get("BUCKET"),
+        ),
+    )
+    while not operation.done:
+        time.sleep(15)
+        operation = client.operations.get(operation)
+        print(operation)
+
+    if operation.response:
+        video_uri = operation.result.generated_videos[0].video.uri
+        tool_context.save_artifact(
+            "video.mp4",
+            types.Part.from_uri(file_uri=video_uri, mime_type="video/mp4"),
+        )
+        return {"status": "ok", "filename": "video.png"}
