33cda0bb2bb7b229bb444ebbf635ced2874b029e
Jeremy Wortz <jwortz@google.com>
2025-04-14 11:46:53 -0500

    trying a moviepy example

--- marketing_idea_generator_agent/common_agents/image_generator/agent.py
+++ marketing_idea_generator_agent/common_agents/image_generator/agent.py
@@ -2,21 +2,30 @@ from .tools import generate_image, generate_video
 from google.adk.agents import Agent
 from google.adk.tools import load_artifacts
 from google.adk.tools import ToolContext, LongRunningFunctionTool
-from .prompts import image_generation_instructions
+from .prompts import (
+    image_generation_instructions,
+    video_generation_tips,
+    movie_code_generation_example,
+)
 from google.genai import types
+from google.adk.code_executors.unsafe_local_code_executor import UnsafeLocalCodeExecutor
+
 
 
 image_generation_agent = Agent(
     model="gemini-2.0-flash-001",
     name="image_generation_agent",
-    instruction=image_generation_instructions,
+    instruction=image_generation_instructions
+    + video_generation_tips
+    + movie_code_generation_example,
     tools=[
         generate_image,
-        generate_video,
+        LongRunningFunctionTool(generate_video),
         load_artifacts,
     ],
     generate_content_config=types.GenerateContentConfig(
         temperature=1.0,
         # response_modalities=["TEXT", "AUDIO"],
     ),
+    code_executor=UnsafeLocalCodeExecutor(),
 )
