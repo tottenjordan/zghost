585cf5e8daa1dc6bb7be36dc2bc114173a794f08
Jeremy Wortz <jwortz@google.com>
2025-04-15 14:52:39 -0500

    added scraping capabilities

--- marketing_idea_generator_agent/common_agents/idea_generator/agent.py
+++ marketing_idea_generator_agent/common_agents/idea_generator/agent.py
@@ -2,14 +2,17 @@ from .prompts import create_brief_prompt
 from google.genai import types
 from google.adk.agents import Agent
 from google.adk.tools import google_search
+from .tools import perform_google_search, extract_main_text_from_url
 
-
+official_tooling = [google_search]
+tools = [perform_google_search, extract_main_text_from_url]
 create_new_ideas_agent = Agent(
     model="gemini-2.0-flash-001",
     name="create_new_ideas_agent",
     instruction=create_brief_prompt,
-    tools=[google_search],
+    tools=tools,
     generate_content_config=types.GenerateContentConfig(
+        http_options=types.HttpOptions(api_version='v1beta1'),
         temperature=1.0,
         # response_modalities=["TEXT", "AUDIO"],
     ),
