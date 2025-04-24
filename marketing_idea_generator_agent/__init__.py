dfea41d7a7ba169563b4c02ff2294d1c9aaa368c
Jeremy Wortz <jwortz@google.com>
2025-04-24 13:22:50 -0500

    cleaned up init

--- marketing_idea_generator_agent/__init__.py
+++ marketing_idea_generator_agent/__init__.py
@@ -1,11 +1,3 @@
-import os
-
-os.environ["GOOGLE_CLOUD_PROJECT"] = "wortz-project-352116"
-os.environ["GOOGLE_CLOUD_PROJECT_NUMBER"] = "679926387543"
-os.environ["GOOGLE_CLOUD_LOCATION"] = "us-central1"
-os.environ["GOOGLE_GENAI_USE_VERTEXAI"] = "1"
-os.environ["BUCKET"] = "gs://zghost-media-center"
-
 from . import agent
 
 __all__ = ["agent"]
