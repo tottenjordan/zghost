7c786c6c86f87ec6a79f2cb75c3db535f0afffd0
Jordan <jordantotten@google.com>
2025-04-28 21:33:49 +0000

    clean up the research gneeration agent files

--- marketing_idea_generator_agent/common_agents/research_generator/prompts.py
+++ marketing_idea_generator_agent/common_agents/research_generator/prompts.py
@@ -4,7 +4,7 @@ Use this tool when the user wants to generate a report artifact from all the cam
 Follow the steps below to generate a research report:
 
 1) Convert {campaign_brief}, {trends}, and {insights} into a string of Markdown format.
-2) Use the `generate_brief_pdf` tool. For the 'markdown_string' input argument, be sure to use the Markdown string created in the previous step.
+2) Use the `generate_research_pdf` tool. For the `markdown_string` input argument, be sure to use the Markdown string created in the previous step.
 3) This tool should save the PDF (locally and in Cloud Storage), save the filepath as an Artifact, and return the filepath
 
 When you have successfully completed these steps, transfer the agent back to the parent agent.
--- marketing_idea_generator_agent/common_agents/research_generator/tools.py
+++ marketing_idea_generator_agent/common_agents/research_generator/tools.py
@@ -2,8 +2,6 @@ import os
 import uuid
 import base64
 import logging
-
-logging.basicConfig(level=logging.INFO)
 from typing import Union, Optional
 from markdown_pdf import MarkdownPdf, Section
 
@@ -11,12 +9,13 @@ from google.adk.tools import ToolContext
 from google.genai import types
 from google import genai
 
+logging.basicConfig(level=logging.INFO)
 client = genai.Client()
 
 from ...utils import upload_file_to_gcs
 
 
-def generate_brief_pdf(
+def generate_research_pdf(
     markdown_string: str,
     tool_context: ToolContext,
 ) -> dict:
@@ -31,7 +30,7 @@ def generate_brief_pdf(
     Returns:
         dict: Status and the location of the PDF.
     """
-    logging.info(f"markdown_string in `generate_brief_pdf`: {markdown_string}")
+    logging.info(f"markdown_string in `generate_research_pdf`: {markdown_string}")
 
     filename = uuid.uuid4()
     filepath = f"{filename}.pdf"
@@ -63,53 +62,10 @@ def generate_brief_pdf(
         # Handle potential storage errors (e.g., GCS permissions)
         logging.exception(f"An unexpected error occurred during artifact save: {e}")
 
-    # upload local file to GCS
-    # gcs_blob_path = upload_filename_blob_to_gcs(source_file_path=filepath)
-    gcs_blob_path = upload_file_to_gcs(
-        file_path=filepath, file_data=document_bytes, content_type="application/pdf"
-    )
-    logging.info(f"Saved PDF to: {gcs_blob_path}")
+    # # upload local file to GCS
+    # gcs_blob_path = upload_file_to_gcs(
+    #     file_path=filepath, file_data=document_bytes, content_type="application/pdf"
+    # )
+    # logging.info(f"Saved PDF to: {gcs_blob_path}")
 
     return {"status": "ok", "filename": filepath}
-
-
-# def upload_pdf_to_gcs(
-#     file_path: str, file_data: bytes, gcs_bucket: str = os.environ.get("BUCKET")
-# ):
-#     """Uploads a file to a GCS bucket.
-
-#     Args:
-#         file_path (str): The path to the file to upload.
-#         gcs_bucket (str): The name of the GCS bucket.
-#     Returns:
-#         str: The GCS URI of the uploaded file.
-#     """
-#     from google.cloud import storage
-
-#     gcs_bucket = gcs_bucket.replace("gs://", "")
-#     storage_client = storage.Client()
-#     bucket = storage_client.bucket(gcs_bucket)
-#     blob = bucket.blob(os.path.basename(file_path))
-#     blob.upload_from_string(file_data, content_type="application/pdf")
-#     return f"gs://{gcs_bucket}/{os.path.basename(file_path)}"
-
-
-# testing save via filename vs bytes
-def upload_filename_blob_to_gcs(
-    source_file_path: str,
-    gcs_bucket: str = os.environ.get("BUCKET"),
-):
-    """
-    Uploads a file to the bucket.
-    """
-    from google.cloud import storage
-
-    storage_client = storage.Client()
-    gcs_bucket = gcs_bucket.replace("gs://", "")
-    destination_blob_name = f"gs://{gcs_bucket}/{os.path.basename(source_file_path)}"
-
-    bucket = storage_client.bucket(gcs_bucket)
-    blob = bucket.blob(destination_blob_name)
-    blob.upload_from_filename(source_file_path)
-    print(f"File {source_file_path} uploaded to {destination_blob_name}.")
-    return destination_blob_name
