6474eb1ea48bb1f92cefb2835cd4ce4926205c72
Jordan <jordantotten@google.com>
2025-06-18 20:57:49 +0000

    remove web researcher part 2

--- trends_and_insights_agent/agent.py
+++ trends_and_insights_agent/agent.py
@@ -14,8 +14,6 @@ from .common_agents.campaign_researcher.agent import campaign_researcher_agent
 from .common_agents.trend_assistant.agent import trends_and_insights_agent
 from .common_agents.ad_content_generator.agent import ad_content_generator_agent
 
-# from .common_agents.web_researcher.agent import web_researcher_agent
-
 from .tools import call_campaign_guide_agent
 from .utils import campaign_callback_function, MODEL
 from .prompts import (
@@ -35,7 +33,6 @@ root_agent = Agent(
         trends_and_insights_agent,
         ad_content_generator_agent,
         report_generator_agent,
-        # web_researcher_agent,
         campaign_researcher_agent,
         yt_researcher_agent,
         gs_researcher_agent,
--- trends_and_insights_agent/common_agents/__init__.py
+++ trends_and_insights_agent/common_agents/__init__.py
@@ -2,8 +2,6 @@ from .report_generator.agent import report_generator_agent
 from .trend_assistant.agent import trends_and_insights_agent
 from .ad_content_generator.agent import ad_content_generator_agent
 from .marketing_guide_data_generator.agent import campaign_guide_data_generation_agent
-
-from .web_researcher.agent import web_researcher_agent
 from .yt_researcher.agent import yt_researcher_agent
 from .gs_researcher.agent import gs_researcher_agent
 from .campaign_researcher.agent import campaign_researcher_agent
@@ -13,7 +11,6 @@ __all__ = [
     "trends_and_insights_agent",
     "ad_content_generator_agent",
     "campaign_guide_data_generation_agent",
-    "web_researcher_agent",
     "yt_researcher_agent",
     "gs_researcher_agent",
     "campaign_researcher_agent",
