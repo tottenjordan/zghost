cfe30c2d632d0086a474f7fb684c48fa38dd7ee8
Jordan <jordantotten@google.com>
2025-06-24 18:05:16 +0000

    uncomment tmp max date

--- trends_and_insights_agent/common_agents/trend_assistant/tools.py
+++ trends_and_insights_agent/common_agents/trend_assistant/tools.py
@@ -145,8 +145,8 @@ def get_daily_gtrends() -> str:
              Returns 25 terms ordered by their rank (ascending order) for the current week.
     """
     # get latest refresh date
-    # max_date = get_gtrends_max_date()
-    max_date = "06/18/2025"
+    max_date = get_gtrends_max_date()
+    # max_date = "06/18/2025"
     logging.info(f"\n\nmax_date in trends_assistant: {max_date}\n\n")
 
     query = f"""
